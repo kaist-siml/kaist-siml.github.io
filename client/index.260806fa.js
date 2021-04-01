27462fe91f76b5f4197bbf4afe74bec6763a19d5
EungGu Yun <yuneg11@gmail.com>
2021-04-01 18:52:38 +0900

    Updates

---
+++ client/index.260806fa.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s}from"./client.7ec1c03a.js";function r(){return this.redirect(301,"/home")}export default class extends t{constructor(t){super(),e(this,t,null,null,s,{})}}export{r as preload};
