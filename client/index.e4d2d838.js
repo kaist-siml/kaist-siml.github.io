dfdb21c8d63f371ffb4ec40be73949c10c13ad3e
GitHub Pages <siml@kaist.ac.kr>
2021-06-30 08:36:30 +0000

    deploy: 98890ccd854c8a8959114a20d927651b181d0386

---
+++ client/index.e4d2d838.js
@@ -0,0 +1,1 @@
+import{S as e,i as t,s}from"./client.e11610de.js";function r(){return this.redirect(301,"/home")}export default class extends e{constructor(e){super(),t(this,e,null,null,s,{})}}export{r as preload};
