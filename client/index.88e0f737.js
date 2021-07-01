8d206d2ad130feb406f46adf7a67c5b75e2a845a
GitHub Pages <siml@kaist.ac.kr>
2021-07-01 04:14:39 +0000

    deploy: 7dcc76f3fca696bf6159e4806d4dbfa19b98c8ad

---
+++ client/index.88e0f737.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as e}from"./client.fff9674b.js";function r(){return this.redirect(301,"/home")}export default class extends t{constructor(t){super(),s(this,t,null,null,e,{})}}export{r as preload};
--- client/index.e4d2d838.js
+++
@@ -1,1 +0,0 @@
-import{S as e,i as t,s}from"./client.e11610de.js";function r(){return this.redirect(301,"/home")}export default class extends e{constructor(e){super(),t(this,e,null,null,s,{})}}export{r as preload};
