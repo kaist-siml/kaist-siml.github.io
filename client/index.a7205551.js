f0b23299787d9717d29b232a494d9a6086a83afc
GitHub Pages <siml@kaist.ac.kr>
2021-10-29 04:57:12 +0000

    deploy: 05011d0577331711275b85458d1be04eebcd3eba

--- client/index.a7205551.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e}from"./client.1931318b.js";function r(){return this.redirect(301,"/home")}export default class extends t{constructor(t){super(),s(this,t,null,null,e,{})}}export{r as preload};
