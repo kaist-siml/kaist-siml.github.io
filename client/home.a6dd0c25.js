dfc596254e1232f98ef60b9e67b0f927bcc33cf6
GitHub Pages <siml@kaist.ac.kr>
2023-02-11 11:23:26 +0000

    deploy: cd8e20eb8d0ad702b06617a18d3375c9b936c13a

---
+++ client/home.a6dd0c25.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as r}from"./client.7cc379cb.js";function c(s,t){return this.redirect(301,"/")}class e extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{e as default,c as preload};
