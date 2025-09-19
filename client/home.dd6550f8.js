4cce97bdbfadd92f0efb4a4dd9c3bff301a96043
GitHub Pages <siml@kaist.ac.kr>
2025-09-19 05:59:06 +0000

    deploy: 3cafddc74253863291ff1526b797e19ffa53e950

---
+++ client/home.dd6550f8.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as r}from"./client.f88a3dab.js";function e(s,t){return this.redirect(301,"/")}class a extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{a as default,e as preload};
